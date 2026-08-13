a064dad26f2afcd7eaa467984dc4b4ee5c1805c0
edutechtammy <teacher.tammy.moore@gmail.com>
2026-08-12 19:13:11 -0500

    Tweaking the cup raise animation.

--- animations/joe/joeLift.js
+++ animations/joe/joeLift.js
@@ -4,21 +4,30 @@ export function buildJoeLift() {
     // Create a paused timeline ready to play forward/reverse on hover
     const liftTL = gsap.timeline({ paused: true, reversed: true });
 
+    //y: -3,
+    //duration: 1.8,
+    //joe-arm-and-cup-group
+    //ease: "sine.inOut"
+
     liftTL
-        // Rotate/lift arm up towards mouth
-        .to("#joe-lower-arm", {
-            rotation: -25,
-            transformOrigin: "bottom right",
-            duration: 0.6,
-            ease: "power2.out"
+        // lift arm up
+        .to("#joe-arm-and-cup-group", {
+            y: -30,
+            x: -2,
+            rotation: 5,
+            duration: 1.0,
+            ease: "sine.inOut"
         })
         // Adjust cup angle so coffee doesn't spill mid-air
-        .to("#joe-cup", {
-            rotation: 10,
-            transformOrigin: "center center",
-            duration: 0.6,
+        .to("#joe-arm", {
+            y: 0,
+            x:-1,
+            rotation: 0,
+            transformOrigin: "top center",
+            duration: 1.0,
             ease: "power2.out"
-        }, 0); // Start at same time as arm movement
+        }, 0); 
+        
 
     return liftTL;
 }
--- animations/joe/joeSip.js
+++ animations/joe/joeSip.js
@@ -1,6 +1,24 @@
 // js/animations/joe/joeSip.js
 
-
 export function buildJoeSip() {
-    return gsap.timeline();
+    // Create a paused timeline ready to play forward/reverse on hover
+    const SipTL = gsap.timeline({ paused: true, reversed: true });
+
+    SipTL
+        // Rotate/lift arm up towards mouth
+        .to("#joe-lower-arm", {
+            rotation: -25,
+            transformOrigin: "bottom right",
+            duration: 0.6,
+            ease: "power2.out"
+        })
+        // Adjust cup angle so coffee doesn't spill mid-air
+        .to("#joe-cup", {
+            rotation: 10,
+            transformOrigin: "center center",
+            duration: 0.6,
+            ease: "power2.out"
+        }, 0); // Start at same time as arm movement
+
+    return SipTL;
 }
