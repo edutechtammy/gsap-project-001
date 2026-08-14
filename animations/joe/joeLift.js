f91138bb783761b615456be1fbe2069c81050d75
edutechtammy <teacher.tammy.moore@gmail.com>
2026-08-13 20:17:19 -0500

    Add the eyebrow raise into the coffee lift animation.

--- animations/joe/joeLift.js
+++ animations/joe/joeLift.js
@@ -4,11 +4,6 @@ export function buildJoeLift() {
     // Create a paused timeline ready to play forward/reverse on hover
     const liftTL = gsap.timeline({ paused: true, reversed: true });
 
-    //y: -3,
-    //duration: 1.8,
-    //joe-arm-and-cup-group
-    //ease: "sine.inOut"
-
     liftTL
         // lift arm up
         .to("#joe-arm-and-cup-group", {
@@ -18,16 +13,30 @@ export function buildJoeLift() {
             duration: 1.0,
             ease: "sine.inOut"
         })
+        // Eyebrows raise simultaneously
+        .to("#his-right-eyebrow", {
+            y: -3,
+            x: -2,
+            rotation: 2,
+            duration: 0.5,
+            ease: "sine.inOut"
+        })
+        .to("#his-left-eyebrow", {
+            y: -3,
+            x: 2,
+            rotation: 2,
+            duration: 0.5,
+            ease: "sine.inOut"
+        }, "<") // "<" aligns start time with the start of the previous tween
         // Adjust cup angle so coffee doesn't spill mid-air
         .to("#joe-arm", {
             y: 0,
-            x:-1,
+            x: -1,
             rotation: 0,
             transformOrigin: "top center",
             duration: 1.0,
             ease: "power2.out"
         }, 0); 
-        
 
     return liftTL;
 }
